fbc2ea7d97213b977bfafe2e05ebebee102f0d5e
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2025-08-27 20:03:20 +0000

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,30 +5,32 @@ export const Hero = () => {
     //bota do chamado para ação (cta = call to action )
     // (call to action) para incentivar o usuario a explorar
     return (
-       <section className="hero">
-        <div className="hero__container">
-            <div className="hero__content">
-                <span className="hero__eyebrow"></span>
-                <h1 className="hero__title"></h1>
-                <p className="hero__subtitle"></p>
-                <div className="hero__actions">
-                    <a href="" className="btn btn--primary"></a>
-                    <a href="" className="btn btn--ghost"></a>
-                    <div className="hero__social-proof">
-                        <div className="hero__avatars">
-                            <img src="" alt="" />
-                            <img src="" alt="" />
-                            <img src="" alt="" />
-                            <img src="" alt="" />
+        <section className="hero">
+            <div className="hero__container">
+                <div className="hero__content">
+                    <span className="hero__eyebrow">NOVIDADES + LANÇAMENTOS</span>
+                    <h1 className="hero__title">
+                        Transforme visitantes em clientes com uma landing page incrivel.
+                    </h1>
+                    <p className="hero__subtitle">Texto claro, visual profissional e CTA direto.</p>
+                    <div className="hero__actions">
+                        <a href="" className="btn btn--primary">Comece agora!</a>
+                        <a href="" className="btn btn--ghost">Ver Desmonstrações</a>
+                        <div className="hero__social-proof">
+                            <div class="hero__avatars">
+                                <img src="https://i.pravatar.cc/56?img=12" alt="Cliente 1" />
+                                <img src="https://i.pravatar.cc/56?img=5" alt="Cliente 2" />
+                                <img src="https://i.pravatar.cc/56?img=33" alt="Cliente 3" />
+                                <img src="https://i.pravatar.cc/56?img=18" alt="Cliente 4" />
                             </div>
-                        <span></span>
+                            <span>3 bilhoes de criadores confiam neste modelo</span>
+                        </div>
+                    </div>
+                    <div className="hero__visual">
+                        <div className="hero__mock"></div>
                     </div>
                 </div>
-                <div className="hero__visual">
-                    <div className="hero__mock"></div>
-                </div>
             </div>
-        </div>
-       </section>
+        </section>
     )
 }
