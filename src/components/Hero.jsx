4acc61408cf0ccc595e7aa47f500808805430847
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2025-08-27 20:06:54 +0000

    hero ajuste

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,21 +16,22 @@ export const Hero = () => {
                     <div className="hero__actions">
                         <a href="" className="btn btn--primary">Comece agora!</a>
                         <a href="" className="btn btn--ghost">Ver Desmonstrações</a>
-                        <div className="hero__social-proof">
-                            <div class="hero__avatars">
-                                <img src="https://i.pravatar.cc/56?img=12" alt="Cliente 1" />
-                                <img src="https://i.pravatar.cc/56?img=5" alt="Cliente 2" />
-                                <img src="https://i.pravatar.cc/56?img=33" alt="Cliente 3" />
-                                <img src="https://i.pravatar.cc/56?img=18" alt="Cliente 4" />
-                            </div>
-                            <span>3 bilhoes de criadores confiam neste modelo</span>
-                        </div>
                     </div>
-                    <div className="hero__visual">
-                        <div className="hero__mock"></div>
+                    <div className="hero__social-proof">
+                        <div class="hero__avatars">
+                            <img src="https://i.pravatar.cc/56?img=12" alt="Cliente 1" />
+                            <img src="https://i.pravatar.cc/56?img=5" alt="Cliente 2" />
+                            <img src="https://i.pravatar.cc/56?img=33" alt="Cliente 3" />
+                            <img src="https://i.pravatar.cc/56?img=18" alt="Cliente 4" />
+                        </div>
+                        <span>3 bilhoes de criadores confiam neste modelo</span>
                     </div>
                 </div>
+                <div className="hero__visual">
+                    <div className="hero__mock"></div>
+                </div>
             </div>
-        </section>
+        </div>
+        </section >
     )
 }
